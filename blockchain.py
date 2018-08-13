77d76dbac8db52c27a6c1f822c9b73dd4110d40d
jreneruiz <jreneruiz20@gmail.com>
2018-08-12 19:12:42 -0600

    Added function to add transactions

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,7 @@
 import hashlib
 import json
 from time import time
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 
 class Blockchain(object):
@@ -76,7 +76,18 @@ def mine():
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    return "Add a new transaction"
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
 
 
 @app.route('/chain', methods=['GET'])
